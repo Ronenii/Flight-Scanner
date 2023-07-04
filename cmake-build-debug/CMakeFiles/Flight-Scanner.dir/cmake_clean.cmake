7b2330cff1d4e9461df8b3daecc61938d485d829
roy12 <roytol12@gmail.com>
2023-07-04 17:00:09 +0300

    fix that shit V4

---
+++ cmake-build-debug/CMakeFiles/Flight-Scanner.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Flight-Scanner.dir/src/child_process.cpp.o"
+  "CMakeFiles/Flight-Scanner.dir/src/child_process.cpp.o.d"
+  "CMakeFiles/Flight-Scanner.dir/src/main.cpp.o"
+  "CMakeFiles/Flight-Scanner.dir/src/main.cpp.o.d"
+  "CMakeFiles/Flight-Scanner.dir/src/parent_process.cpp.o"
+  "CMakeFiles/Flight-Scanner.dir/src/parent_process.cpp.o.d"
+  "CMakeFiles/Flight-Scanner.dir/src/program_manager.cpp.o"
+  "CMakeFiles/Flight-Scanner.dir/src/program_manager.cpp.o.d"
+  "CMakeFiles/Flight-Scanner.dir/src/programs.cpp.o"
+  "CMakeFiles/Flight-Scanner.dir/src/programs.cpp.o.d"
+  "Flight-Scanner"
+  "Flight-Scanner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Flight-Scanner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/lib/CMakeFiles/lib.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/roy12/CLionProjects/Linux-Assignment/Flight-Scanner/lib/AirportFlights.cpp" "lib/CMakeFiles/lib.dir/AirportFlights.cpp.o" "gcc" "lib/CMakeFiles/lib.dir/AirportFlights.cpp.o.d"
+  "/home/roy12/CLionProjects/Linux-Assignment/Flight-Scanner/lib/Flight.cpp" "lib/CMakeFiles/lib.dir/Flight.cpp.o" "gcc" "lib/CMakeFiles/lib.dir/Flight.cpp.o.d"
+  "/home/roy12/CLionProjects/Linux-Assignment/Flight-Scanner/lib/Functions.cpp" "lib/CMakeFiles/lib.dir/Functions.cpp.o" "gcc" "lib/CMakeFiles/lib.dir/Functions.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
